13addf4adc8ba7f5821b6221897ee5619f797bd2
Richard McKnight <rich@Richards-MBP-2.lan>
2024-11-28 18:11:45 -0500

    deletes

--- fastapi/rag_llms.py
+++
@@ -1,6 +0,0 @@
-import json
-import os
-## This is used to do any environmental set up
-CONFIG = json.load(open("llm_config.json"))
-openai_api_key = open(f'{CONFIG["KEY_FOLDER"]}/{CONFIG["OPENAI_API_KEY_FILE"]}').read().strip()
-os.environ["OPENAI_API_KEY"] = openai_api_key
