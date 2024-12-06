db8bd7e4344b8a9da657091c70e37ffa8d8c9111
Richard McKnight <rich@Richards-MBP-2.lan>
2024-12-05 21:47:47 -0500

    respectful and helpful

--- fastapi/openai_ragifier.py
+++ fastapi/openai_ragifier.py
@@ -166,8 +166,8 @@ class OpenAIRAGifier(RAGIfier):
         ragified_prompt = {
             "system_prompt": """Please use the 'relevant_content' to answer the question posed by the prompt. 
 Higher 'relevance' implies greater importance. 
-If the prompt implies knowlege from a previous question, please remind the user in a mildly 
-self-depracating and humorous manner that you have no recollection of previous questions and answers.""",
+If the prompt implies knowlege from a previous question, please remind the user in a respectul and  
+helpful manner that you have no recollection of previous questions and answers.""",
             "prompt": prompt,
             "retrieval": retrieval,
             "relevant_content": raw_docs
